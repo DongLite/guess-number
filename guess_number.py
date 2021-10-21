f1ca97de18e01ac1322c787e715837a87f470c9d
Dong Lite <pp5743218@gmail.com>
2021-10-21 15:31:57 +0800

    增加猜測次數提醒

--- guess_number.py
+++ guess_number.py
@@ -10,14 +10,19 @@
 
 import random
 r = random.randint(1, 100)
+count = 0
 
 while True:
+	count += 1
 	num = input('請猜1~100其中的數字：')
 	num = int(num)
 	if num == r:
 		print('恭喜猜中了！')
+		print('共花了', count, '次')
 		break
 	elif num > r:
-		print('比目標數字還要大')
+		print('---比目標數字還要大---')
 	elif num < r:
-		print('比目標數字還要小')
+		print('---比目標數字還要小---')
+	print('這是你猜的第', count, '次')
+	print('')
